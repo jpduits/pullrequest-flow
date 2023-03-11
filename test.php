5f98a80d806e15f62adee92424a1c96cd823ccd6
jpduits <duits.jp@gmail.com>
2023-03-11 14:22:21 +0100

    Test branch 2 (#6)
    * Add line 2
    
    * Add line 2
    
    ---------
    
    Co-authored-by: jpduits <git@jpduits.nl>

--- test.php
+++ test.php
@@ -6,3 +6,7 @@ echo 'Aanpassing voor pullrequest';
 
 echo 'Aanpassing in testbranch';
 echo 'Nog een aanpassing, hoeveel commits zijn er nu bijgekomen?';
+
+echo 'Nu gaan we andere manier van mergen proberen';
+
+echo 'Aanpassing voor pullrequest';
