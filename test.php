1808717b107a780b8423e60ad1a6ea113a0d246f
jpduits <git@jpduits.nl>
2023-03-11 14:20:37 +0100

    Add line 2

--- test.php
+++ test.php
@@ -6,3 +6,5 @@ echo 'Aanpassing voor pullrequest';
 
 echo 'Aanpassing in testbranch';
 echo 'Nog een aanpassing, hoeveel commits zijn er nu bijgekomen?';
+
+echo 'Nu gaan we andere manier van mergen proberen';
